d14207ac6ed26a1f8eed914e0b1ef89938c792c2
Alberto De Bortoli <albertodebortoli@gmail.com>
2013-02-23 17:12:09 +0100

    Initial Commit

---
+++ ADBBackgroundCells/ADBAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ADBAppDelegate.h
+//  ADBBackgroundCells
+//
+//  Created by Alberto De Bortoli on 2/23/13.
+//  Copyright (c) 2013 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ADBViewController;
+
+@interface ADBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ADBViewController *viewController;
+
+@end
---
+++ ADBBackgroundCells/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADBBackgroundCells
+//
+//  Created by Alberto De Bortoli on 2/23/13.
+//  Copyright (c) 2013 Alberto De Bortoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ADBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ADBAppDelegate class]));
+    }
+}
